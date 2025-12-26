0d5b797927fb0ecbfb34380a3387bcd52dfd9ab0
Anastasia Malkova <ehznla@mail.ru>
2025-12-26 15:12:31 +0300

    maybe now

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -8,7 +8,7 @@ import useEmblaCarousel, {
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { CarouselArrowLeft } from "../icons/CarouselArrowLeft"
-import { CarouselArrowRight } from "../icons/CarouselArrowRight"
+import { CarouselArrowRight } from "../icons/CarouselArrow"
 
 type CarouselApi = UseEmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
