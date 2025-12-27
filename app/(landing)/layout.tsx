524f94ab02cbcc9dccb1895a4a02a05568f25886
Anastasia Malkova <ehznla@mail.ru>
2025-12-27 20:51:46 +0300

    final version 4

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<main className=" lg:px-36 px-4">
+		<main className="container mx-auto lg:px-36 px-4">
 			<Header />
 			{children}
 		</main>
